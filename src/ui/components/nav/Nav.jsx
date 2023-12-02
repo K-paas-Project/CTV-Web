deee0b1f9b657def8b1f6ace06c5893961d3caa1
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-03 02:02:34 +0900

    Add :: Navigator

--- src/ui/components/nav/Nav.jsx
+++ src/ui/components/nav/Nav.jsx
@@ -1,8 +1,32 @@
-import {Container} from "./NavStyle";
+import {Container, BurgerContainer, HomeContainer, LeftContent, RightContent, LoginContainer} from "./NavStyle";
+import CTVIcon from "../icon/Icon";
+import CTVButton from "../button/Button";
+import IcBurger from '../../../asset/ic_burger.svg';
+import IcLogo from '../../../asset/ic_logo.svg';
+import C2VText from "../text/Text";
 
 export default function Nav() {
   return (
     <Container>
+      <LeftContent>
+        <BurgerContainer>
+          <CTVButton onClick={() => console.log('se')}>
+            <CTVIcon src={IcBurger}/>
+          </CTVButton>
+        </BurgerContainer>
+        <HomeContainer>
+          <CTVButton onClick={() => console.log('to home')}>
+            <CTVIcon src={IcLogo}/>
+          </CTVButton>
+        </HomeContainer>
+      </LeftContent>
+      <RightContent>
+        <LoginContainer>
+          <CTVButton onClick={() => console.log('login')}>
+            <C2VText text={'로그인'} type={'body'}/>
+          </CTVButton>
+        </LoginContainer>
+      </RightContent>
 
     </Container>
   );
--- src/ui/components/nav/NavStyle.js
+++ src/ui/components/nav/NavStyle.js
@@ -5,5 +5,29 @@ export const Container = styled.div`
   width: 100%;
   height: 72px;
   box-shadow: 2px 2px 8px 0 #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const BurgerContainer = styled.div`
+  margin-left: 24px;
   
+`;
+
+
+export const HomeContainer = styled.div`
+  margin-left: 16px;
+`
+
+export const LeftContent = styled.div`
+  display: flex;
+`
+
+export const RightContent = styled.div`
+  display: flex;
+`
+
+export const LoginContainer = styled.div`
+  margin-right: 32px;
 `
