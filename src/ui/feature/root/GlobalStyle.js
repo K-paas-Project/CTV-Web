27d23a35405dc0a310bcf807766ec75449e5061f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-03 02:02:24 +0900

    Fix :: Box-sizing

--- src/ui/feature/root/GlobalStyle.js
+++ src/ui/feature/root/GlobalStyle.js
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Pretendard-Regular', sans-serif;
     padding: 0;
     margin: 0;
+    box-sizing: border-box;
   }
   
   @font-face {
