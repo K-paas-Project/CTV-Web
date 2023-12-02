9b51afd8c868a8e88206da0c03a065f12891bdc2
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-03 01:31:02 +0900

    Add :: Routing

--- src/ui/feature/root/App.jsx
+++ src/ui/feature/root/App.jsx
@@ -1,11 +1,19 @@
 import {AppContainer} from "./AppStyle";
 import {GlobalStyle} from "./GlobalStyle";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "../home/Home";
+import UiTest from "../uitest/UiTest";
 
 function App() {
   return (
     <AppContainer>
       <GlobalStyle/>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Home/>}></Route>
+          <Route path={'/ui-test'} element={<UiTest/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </AppContainer>
   );
 }
