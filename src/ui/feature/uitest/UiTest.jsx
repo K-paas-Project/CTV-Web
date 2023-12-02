c6062d6c38930a385adf18297b6e005dceb70903
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-03 01:31:07 +0900

    Init :: Ui Test

---
+++ src/ui/feature/uitest/UiTest.jsx
@@ -0,0 +1,14 @@
+import C2VText from "../../components/text/Text";
+
+export default function UiTest() {
+  return (
+    <>
+      <C2VText text={'sex'} type={'title'}/>
+      <C2VText text={'sex'} type={'headline'}/>
+      <C2VText text={'sex'} type={'body'}/>
+      <C2VText text={'sex'} type={'label'}/>
+      <C2VText text={'sex'} type={'caption'}/>
+      <C2VText text={'sex'} type={'mini'}/>
+    </>
+  )
+}
