1692556c7c35b972ec28ce4de7c095d8b00beed3
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-03 01:02:13 +0900

    Add :: GlobalStyle

---
+++ src/ui/feature/root/App.jsx
@@ -0,0 +1,13 @@
+import {AppContainer} from "./AppStyle";
+import {GlobalStyle} from "./GlobalStyle";
+
+function App() {
+  return (
+    <AppContainer>
+      <GlobalStyle/>
+      
+    </AppContainer>
+  );
+}
+
+export default App;
---
+++ src/ui/feature/root/GlobalStyle.js
@@ -0,0 +1,16 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Pretendard-Regular', sans-serif;
+    padding: 0;
+    margin: 0;
+  }
+  
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+`
