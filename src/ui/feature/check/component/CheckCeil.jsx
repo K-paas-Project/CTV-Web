4044825fe34caf3de2952d8dc8550a2265206520
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-12-08 21:52:41 +0900

    Fix :: date text

--- src/ui/feature/check/component/CheckCeil.jsx
+++ src/ui/feature/check/component/CheckCeil.jsx
@@ -3,6 +3,10 @@ import C2VText from "../../../components/text/Text";
 import Color from "../../../components/theme/color/Color";
 
 export default function CheckCeil({model, callback}) {
+
+  const D = new Date(model.dateTime);
+  const dateText = `${D.getFullYear()}-${D.getMonth()}-${D.getDay()} ${D.getHours()}:${D.getMinutes()}:${D.getSeconds()}`
+
   return (
     <Container onClick={callback}>
       <div>{model.category}</div>
@@ -10,7 +14,7 @@ export default function CheckCeil({model, callback}) {
         <C2VText text={model.title} type={'body'} weight={700}/>
         <DetailContent>
           <C2VText text={model.content} type={'label'} color={Color.gray3}/>
-          <C2VText text={model.dateTime} type={'label'} color={Color.gray3}/>
+          <C2VText text={dateText} type={'label'} color={Color.gray3}/>
         </DetailContent>
       </DownContent>
     </Container>
