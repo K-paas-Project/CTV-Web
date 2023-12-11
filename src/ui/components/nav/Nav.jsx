d7081699952b150b590daa1818c72a49048cf887
bswlkh0310 <hhhello0507@gmail.com>
2023-12-11 12:31:25 +0900

    Fix :: if error then to login view

--- src/ui/components/nav/Nav.jsx
+++ src/ui/components/nav/Nav.jsx
@@ -6,8 +6,20 @@ import IcLogo from '../../../asset/ic_logo.svg';
 import C2VText from "../text/Text";
 import {useNavigate} from "react-router-dom";
 import {Url} from "../../feature/root/App";
+import {useEffect, useState} from "react";
+import {getProfile} from "../../../data/service/api/AuthApi";
 
 export default function Nav({callback}) {
+
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    getProfile()
+      .then(() => {
+        setIsLogin(true);
+      })
+      .catch(() => {});
+  }, []);
 
   const navigate = useNavigate();
   return (
@@ -28,9 +40,9 @@ export default function Nav({callback}) {
         </LeftContent>
         <RightContent>
           <LoginContainer>
-            <CTVButton onClick={() => navigate(Url.login)}>
+            {!isLogin ? <CTVButton onClick={() => navigate(Url.login)}>
               <C2VText text={'로그인'} type={'body'}/>
-            </CTVButton>
+            </CTVButton> : null}
           </LoginContainer>
         </RightContent>
       </Container>
